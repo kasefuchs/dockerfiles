51f30870c099697f86ef7a3b769813d3433228e3
kasefuchs <kasefuchs@protonmail.com>
2025-12-11 18:02:13 +0300

    remove: remove copywrite

--- stash/rootfs/usr/local/bin/gocryptfs-ready.sh
+++ stash/rootfs/usr/local/bin/gocryptfs-ready.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-# Copyright (c) Kasefuchs
-# SPDX-License-Identifier: MIT
-
 set -eu
 
 GOCRYPTFS_MOUNT_PATH="${GOCRYPTFS_MOUNT_PATH:-/decrypted}"
--- stash/rootfs/usr/local/bin/stash-start.sh
+++ stash/rootfs/usr/local/bin/stash-start.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-# Copyright (c) Kasefuchs
-# SPDX-License-Identifier: MIT
-
 set -eu
 
 /usr/local/bin/gocryptfs-ready.sh && exec /usr/bin/stash
