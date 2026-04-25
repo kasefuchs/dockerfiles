3cdd9b20dc822f8f3b5bf3eafed44bdab402bef8
kasefuchs <kasefuchs@protonmail.com>
2026-04-25 20:06:27 +0300

    feat: split base and apps

--- apps/stash/rootfs/usr/local/bin/gocryptfs-ready.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -eu
-
-GOCRYPTFS_MOUNT_PATH="${GOCRYPTFS_MOUNT_PATH:-/decrypted}"
-
-exec mountpoint -q "$GOCRYPTFS_MOUNT_PATH"
--- apps/stash/rootfs/usr/local/bin/stash-start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-set -eu
-
-/usr/local/bin/gocryptfs-ready.sh && exec /usr/bin/stash
